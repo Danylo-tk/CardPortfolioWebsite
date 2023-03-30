7c1e48545821f8a2267919ecbfdbf54eba4c48b0
Danylo Tkach <danylo.tkach@gmail.com>
2023-03-30 22:11:42 +0300

    Added navigation links

--- app/page.tsx
+++ app/page.tsx
@@ -4,23 +4,40 @@ import PerlinBg from "./components/PerlinBg";
 import styles from "./page.module.css";
 
 import myImg from "../public/me.png";
+import LinkBox from "./components/LinkBox";
 
 export default function Home() {
   return (
     <main>
       <PerlinBg />
       <MainBg>
-        <div className={styles.description}>
-          <div>
-            <div className={styles.hiContainer}>
-              <h1 className={styles.header}>H</h1>
-              <Image src={myImg} alt="Picture of the author." />
+        <div className={styles.container}>
+          <div className={styles.descriptionSection}>
+            <div>
+              <div className={styles.hiContainer}>
+                <h1 className={styles.header}>H</h1>
+                <Image src={myImg} alt="Picture of the author." />
+              </div>
+              <h2 className={styles.subHeader}>I'm Dan!</h2>
             </div>
-            <h2 className={styles.subHeader}>I'm Dan!</h2>
+            <div>
+              <p className={styles.aboutText}>Front-end web developer</p>
+              <p className={styles.aboutText}>Lviv, Ukraine</p>
+            </div>
           </div>
-          <div>
-            <p className={styles.aboutText}>Front-end web developer</p>
-            <p className={styles.aboutText}>Lviv, Ukraine</p>
+          <div className={styles.linksSection}>
+            <h1 className={styles.aboutText}>Let's connect!</h1>
+            <ul className={styles.linksList}>
+              <li>
+                <LinkBox>Linkedin 💼</LinkBox>
+              </li>
+              <li>
+                <LinkBox>Github 👾</LinkBox>
+              </li>
+              <li>
+                <LinkBox>My resume 📜</LinkBox>
+              </li>
+            </ul>
           </div>
         </div>
       </MainBg>
